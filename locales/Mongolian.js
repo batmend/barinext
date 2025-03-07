c70ea017262867293c8ccda539a2b90b8a4ec3fd
Batmend Ganbat <batmend.g@gmail.com>
2025-03-07 22:14:23 +0800

    test

--- locales/Mongolian.js
+++ locales/Mongolian.js
@@ -11,7 +11,7 @@ const Mongolian = {
   FooterOtherLinks: "БУСАД ХОЛБООС",
 
   BariService: "БАРИ үйлчилгээ",
-  BariPortfolio: "САЙН УУ1 ДЭЛХИЙ МИНЬ",
+  BariPortfolio: "САЙН УУ12 ДЭЛХИЙ МИНЬ",
 
   aboutUs: "Бидний тухай",
   aboutUsDesc:
